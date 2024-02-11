c70bd171420850c91bbb8339e41f5fba3c4912ba
Nishant kumar <nishant51121@gmail.com>
2024-02-11 18:16:34 +0530

    make it more interactive and added image if cart is empty

--- Projects/data/priceManager.js
+++ Projects/data/priceManager.js
@@ -1,5 +1,6 @@
 import { products } from "../data/product.js";
 import {cart} from '../data/cart.js';
+import { renderOrderSummary } from "../../script/checkout.js";
 
 export function updatePrice() {
     let totalValue = 0;
@@ -21,10 +22,18 @@ export function updatePrice() {
     cart.forEach((quantity) => {
         totalValue *= quantity.quantity;
     })
-    
-    
 
+    if (totalValue === 0) {
+        document.querySelector('.empty-cart').innerHTML = `
+        <div class="product-box">
+            <img src="https://www.iconeasy.com/icon/256/System/Swirl%20Finder/Finder%20Candy.png">
+        </div>
+        `;
+    } else {
+        true;
+    }
     document.querySelector('.js-total-amount').innerHTML = `$${((totalValue)/100).toFixed(2)}`;
     document.querySelector('.js-total-amounts').innerHTML = `$${(totalValue)/100}`;
     document.querySelector('.js-total-amountss').innerHTML = `$${((totalValue)/100).toFixed(2)}`;
+    
 }
--- script/checkout.js
+++ script/checkout.js
@@ -6,72 +6,76 @@ import { updatePrice } from '../Projects/data/priceManager.js';
 const today = dayjs();
 let deliveyDate = today.add(7,'days');
 deliveyDate = deliveyDate.format('DD MMM, dddd');
-console.log(deliveyDate);
 
+export function renderOrderSummary(){
 
-let cartSummaryHtml = '';
-cart.forEach((cartItem) => {
+    let cartSummaryHtml = '';
+    cart.forEach((cartItem) => {
 
-    const productId = cartItem.productId;
+        const productId = cartItem.productId;
 
-    let matchingProduct;
+        let matchingProduct;
 
-    products.forEach((product) => {
-        if (product.productId === productId) {
-            matchingProduct = product;
-        }
-    });
+        products.forEach((product) => {
+            if (product.productId === productId) {
+                matchingProduct = product;
+            }
+        });
 
-    cartSummaryHtml +=
-    `
-    <div class="product-box js-product-box-${matchingProduct.productId}">
-        <div class="product1">
-            <div class="image">
-                <img src="${matchingProduct.image}" alt="">
-            </div>
-            <div class="details">
-                <div class="product-details">
-                    <p class="product-name">${matchingProduct.name}</p>
-                    <input placeholder="${cartItem.quantity}" type="number">
-                    <div>
-                        <p>$${(matchingProduct.priceCents/100).toFixed(2)}</p>
-                        <button class="js-delete-link update-price" data-product-id="${matchingProduct.productId}">Remove</button>
+        cartSummaryHtml +=
+        `
+        <div class="product-box js-product-box-${matchingProduct.productId}">
+            <div class="product1">
+                <div class="image">
+                    <img src="${matchingProduct.image}" alt="">
+                </div>
+                <div class="details">
+                    <div class="product-details">
+                        <p class="product-name">${matchingProduct.name}</p>
+                        <p>${cartItem.quantity}</p>
+                        <div>
+                            <p>$${(matchingProduct.priceCents/100).toFixed(2)}</p>
+                            <button class="dark-back js-delete-link update-price" data-product-id="${matchingProduct.productId}">Remove</button>
+                        </div>
                     </div>
-                </div>
-                <div class="addition">
-                    <p>
-                        Add gift wrap and letter for someone <span style="font-weight:bold;">$3/-</span></p>
-                        <button>Add</button>
-                </div>
-                <div class="order">
-                    <p>Order today.Delivers to 400011††
-                        <span style="color:green; font-weight:bolder;">${deliveyDate}— Free</span></p>
-                    <p>Order now. Pick up, in-store:
-                        Today at Store.</p>
+                    <div class="addition">
+                        <p>
+                            Add gift wrap and letter for someone <span style="font-weight:bold;">$3/-</span></p>
+                            <button>Add</button>
+                    </div>
+                    <div class="order">
+                        <p>Order today.Delivers to 400011††
+                            <span style="color:green; font-weight:bolder;">${deliveyDate}— Free</span></p>
+                        <p>Order now. Pick up, in-store:
+                            Today at Store.</p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    `;
-});
-
-document.querySelector('.js-order-summary').innerHTML = cartSummaryHtml;
-updatePrice();
-document.querySelectorAll('.update-price').forEach((element) => {
-    element.addEventListener('click', () => {
-        updatePrice();
-    })
-})
-
-
-document.querySelectorAll('.js-delete-link')
-    .forEach((link) => {
-        link.addEventListener('click', () => {
-            const productId = link.dataset.productId;
-            removeFromCart(productId);
-            
-
-            const container = document.querySelector(`.js-product-box-${productId}`);
-            container.remove();
-        });
+        `;
     });
+
+    document.querySelector('.js-order-summary').innerHTML = cartSummaryHtml;
+    updatePrice();
+
+    document.querySelectorAll('.js-delete-link')
+        .forEach((link) => {
+            link.addEventListener('click', () => {
+                const productId = link.dataset.productId;
+                removeFromCart(productId);
+                
+
+                const container = document.querySelector(`.js-product-box-${productId}`);
+                container.remove();
+            });
+        });
+
+    document.querySelectorAll('.update-price').forEach((element) => {
+        element.addEventListener('click', () => {
+            updatePrice();
+            renderOrderSummary();
+        })
+    })
+};
+
+renderOrderSummary();
