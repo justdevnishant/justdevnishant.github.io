e5ee25b1ea593a10d15e94eaa07a87af06a4102a
Nishant kumar <nishant51121@gmail.com>
2024-03-16 16:35:21 +0530

    data for project card

---
+++ data/project-datails.js
@@ -0,0 +1,50 @@
+/* ---------------------------Products data-------------------------- */
+export const projectData = [{
+    link:'https://github.com/nishant0kumar/nishant0kumar.github.io',
+    image: 'project1.jpeg',
+    name: 'Initial Portfolio',
+    productDescreption :`It's a just a simple web page, when I learned something I make change in this as well as I show how much I have learned. This is font page of my Portfolio.`,
+    languageUsed:'#html #css #js',
+    productId: 'a1a2'
+},
+{
+    link:'https://github.com/nishant0kumar/nishant0kumar.github.io',
+    image:'project2.jpeg',
+    name: 'Initial Portfolio',
+    productDescreption:`It'a just a simple web page, when I learned something I make change in this as well as I show how much I have learned. This is initial project section here you can my project and on which I am currently working.`,
+    languageUsed:'#html #css #js',
+    productId: 'a3a4'
+},
+{
+    link:'https://github.com/nishant0kumar/nishant0kumar.github.io/tree/master/Projects',
+    image:'web-page-light.jpeg',
+    name: 'Candy Shop',
+    productDescreption:`Building a web application where user can add candies in their cart and place a order. {Prototype #1}`,
+    languageUsed:'#html #css #js',
+    productId: 'b1b2'
+},
+{
+    link:'',
+    image:'candy.jpeg',
+    name: 'Candy Shop',
+    productDescreption:`Building a web application where user can add candies in their cart and place a order. {Prorotype #2}`,
+    languageUsed:'#html #css #js',
+    productId: 'b3b4'
+},
+{
+    link:'',
+    image:'sweet.jpeg',
+    name: 'Sweet Candy',
+    productDescreption:``,
+    languageUsed:'#html #css',
+    productId: 'b5b6'
+},
+{
+    link:'https://github.com/nishant0kumar/nishant0kumar.github.io/tree/master/Projects',
+    image:'web-page-dark.jpeg',
+    name: 'Candy Shop',
+    productDescreption:``,
+    languageUsed:'',
+    productId: 'b1b2b3'
+},
+];
